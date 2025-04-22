584929b003cc8eea1b770c66bff5d2e9153aa5c8
DebjitRoy <debjitroy.dev@gmail.com>
2025-04-22 00:05:00 -0500

    stubbing routes and server actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,10 +1,7 @@
-'use server';
+//importing and exporting names functions from index
+export { signIn } from './sign-in';
+export { signOut } from './sign-out';
 
-import * as auth from '@/auth';
-
-export async function signIn() {
-  return auth.signIn('github');
-}
-export async function signOut() {
-  return auth.signOut();
-}
+export { createComment } from './create-comment';
+export { createPost } from './create-post';
+export { createTopic } from './create-topic';
